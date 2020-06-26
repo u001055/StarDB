9812dc0810c2d86f3167001e22ea48c3ba3a3a52
Oleg <52830599+u001055@users.noreply.github.com>
2020-06-26 13:50:50 +0300

    first

---
+++ src/services/swapi-service.js
@@ -0,0 +1,57 @@
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.dev/api'
+  
+    async getResource(url) {
+      const res = await fetch(`${this._apiBase}${url}`);
+  
+      if (!res.ok) {
+        throw new Error('404')
+      }
+  
+      return await res.json();
+    };
+  
+    async getAllPeople() {
+      const res = await this.getResource(`/people/`);
+      return res.results;
+    }
+  
+    getPerson(id) {
+      return this.getResource(`/people/${id}`);
+    }
+  
+    async getAllPlanet() {
+      const res = await this.getResource(`/planets/`);
+      return res.results.map(this._transformPlanet);
+    }
+  
+    async getPlanet(id) {
+      const planet = await this.getResource(`/planets/${id}`);
+      return this._transformPlanet(planet);
+    }
+  
+    async getAllStarships() {
+      const res = await this.getResource(`/starships/`);
+      return res.results;
+    }
+
+    getStarship(id) {
+      return this.getResource(`/starships/${id}`);
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        }
+    }
+  };
